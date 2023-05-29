77d33260103045f8ad5b51f033106524149c950f
Nikita Koval <ndkoval@ya.ru>
2023-05-29 16:19:22 +0300

    Day 1

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.18.3")
+    }
+}
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    java
+}
+apply(plugin = "kotlinx-atomicfu")
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    testImplementation(kotlin("test-junit"))
+    testImplementation("org.jetbrains.kotlinx:lincheck:2.16")
+    testImplementation("com.amazonaws:aws-java-sdk:1.12.429")
+}
+
+tasks {
+    test {
+        maxHeapSize = "6g"
+    }
+}
+
+sourceSets.main {
+    java.srcDir("src")
+}
+
+sourceSets.test {
+    java.srcDir("test")
+}
+
+java {
+    toolchain {
+        // TODO change to Java 13, because of spins in novel AQS
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.withType<Test> {
+    jvmArgs(
+        "--add-opens", "java.base/jdk.internal.misc=ALL-UNNAMED",
+        "--add-exports", "java.base/jdk.internal.util=ALL-UNNAMED",
+        "--add-exports", "java.base/sun.security.action=ALL-UNNAMED"
+    )
+}
